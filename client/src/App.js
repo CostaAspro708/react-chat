dff06f5509c0ed4fdea8ee0e5fc8e40e417681e2
CostaAspro708 <n10464174@qut.edu.au>
2023-04-29 23:28:02 +1000

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:4000');
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
